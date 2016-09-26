cbf306da87e4bb16abfa66c61eb629028bfd0e1b
DESKTOP-NH5JUA2\Владислав <bus_ter2011@mail.ru>
2016-09-26 12:57:27 +0300

    123

--- scenes/tutorials.lua
+++ scenes/tutorials.lua
@@ -6,10 +6,10 @@
 
 -- ФУНКЦИИ
 function createArrow(tutorialX,tutorialY,tutorialR)
---Com
+
   imgArrow = display.newImage( "img/arrow.png", tutorialX, tutorialY );
   animateChange = false;
-
+  
   local function animationArrowHorizontal()
     if animateChange == true then
       transition.to( imgArrow, { time=500, x=tutorialX } );
